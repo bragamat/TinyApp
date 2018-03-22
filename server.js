435686b554e2fe36be34b4e3d551dbe492b57f6d
bragamat <mateuscbraga@gmail.com>
2018-03-22 18:10:55 +0000

    a lot of problems

--- server.js
+++ server.js
@@ -12,6 +12,18 @@ let urlDatabase = {
   "b2xVn2": "http://lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
 
 const bodyParser = require("body-parser");
 
@@ -26,7 +38,8 @@ app.get("/urls", (req, res) => {
   let templateVars = { 
     title: title, 
     urls: urlDatabase,
-    username: req.cookies["username"] 
+    id:req.cookies.users_id,
+    email: req.cookies.users_email
   };
   res.render('index', templateVars);
   // console.log(title, templateVars);
@@ -35,8 +48,8 @@ app.get("/urls", (req, res) => {
 app.get('/urls/new', (req, res) =>{
 
   let templateVars={
-    username: req.cookies["username"] 
-  } ;
+    user_id: req.cookies["username"] 
+  };
 
   res.render('urls_new', templateVars);
 });
@@ -48,11 +61,7 @@ app.post('/urls/new', (req, res)=>{
   res.redirect('/urls');
 });
 app.post('/urls/:id/delete', (req, res) => {
-  
-// console.log(urlDatabase);
   delete urlDatabase[req.params.id];
-  console.log(urlDatabase);
-
   res.redirect('/urls');
 });
 
@@ -74,24 +83,82 @@ app.get('/urls/:id', (req, res) =>{
 
 app.post('/urls/:id/update', (req, res) =>{
   let newURL = req.body.newURL;
-    console.log(newURL);
     urlDatabase[req.params.id] = newURL;
   res.redirect('/urls');
 });
 
 app.post('/login', (req, res) => {
-res.cookie('username', req.body.username);
-// let templateVars = {
-//   username: req.cookies['username'],
-// };
-  // res.render("index", templateVars);
+   if(req.body.username == "" || req.body.password == ""){
+    res.status(400).send("You need to fill the blanks with text and/or numbers");
+  }
+  for (var ids in users){
+    for (var email in users[ids]){
+      if(req.body.username == users[ids].email && req.body.password == users[ids].password){
+        console.log(req.body.username,"==?", users[ids].email);
+        console.log(req.body.password,"==?", users[ids].password);
+        res.redirect('/urls');
+        continue;
+      }else {
+        console.log(req.body.username,"==?", users[ids].email);
+        res.status(403).send("email and password problems");
+      }
+    } 
+  };
+  // res.cookie('username', req.body.username);
     res.redirect('/urls');
 });
 
 app.post('/logout', (req, res)=>{
-  res.clearCookie('username');
+  res.clearCookie('users_id');
+
   res.redirect('/urls');
 });
+
+app.get('/register', (req, res) =>{
+  let usersVars = {
+    users: users
+  };
+  // console.log(users);
+  res.render('registration', usersVars);
+
+});
+app.get('/registration', (req, res) =>{
+  let usersVars = {
+    users: users
+  };
+  // console.log(users);
+  res.render('registration', usersVars);
+
+});
+
+app.post('/register', (req, res)=>{
+  for (var ids in users) {
+    for(var email in users[ids]){
+      if(req.body.email == users[ids][email]){
+        res.status(400).send("Email already in use");
+      }
+    }
+  }
+  if(req.body.email == "" || req.body.password == ""){
+    res.status(400).send("You need to fill the blanks with text and/or numbers");
+  } else {
+let email = req.body.email;
+let password =  req.body.password;
+let username = req.body.username;
+let id = generateRandomString();
+        let person = {
+          id: id,
+          email: email,
+          password: password
+        };
+    users[id] = person;
+    res.cookie("users_id", id);
+    res.cookie("users_email", email);
+    console.log(users);
+    }
+  res.redirect('/urls');
+}); 
+
 app.listen(PORT, () =>{
   console.log(`${PORT} is the magic port`);
 });
