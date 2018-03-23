d4cfdda3e828b99d2d72c6dd44bc7a3766a3ded4
bragamat <mateuscbraga@gmail.com>
2018-03-23 18:46:22 +0000

    passwords are being stored hasheds - need open the index for everyone

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = process.env.PORT || 8080;
+const bcrypt = require('bcrypt');
+
+function hashPassword(password){
+  let hashedPassword = bcrypt.hashSync(password, 10);
+    return hashedPassword;
+}
+
 
 generateRandomString = () =>{
   var m = m || 6; s = '', r = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -9,19 +16,19 @@ generateRandomString = () =>{
   return s;
 };
 let urlDatabase = {
-  "b2xVn2": "http://lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+ "b2xVn2": {url: "http://www.lighthouselabs.ca", user_id: 'userRandomID'},
+ "9sm5xK": {url: "http://www.google.com", user_id: 'user2RandomID'},
 };
 const users = { 
   "userRandomID": {
     id: "userRandomID", 
     email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
+    password: hashPassword("purple-monkey-dinosaur")
   },
  "user2RandomID": {
     id: "user2RandomID", 
     email: "user2@example.com", 
-    password: "dishwasher-funk"
+    password: hashPassword("123")
   }
 };
 
@@ -35,38 +42,50 @@ app.set('view engine', 'ejs');
 
 app.get("/urls", (req, res) => {
   let title = "My URL's";
+  let urls = {};
+  for (let url in urlDatabase) {
+    let obj = urlDatabase[url];
+    if (obj.user_id === req.cookies.user_id) {
+      urls[url] = obj;
+    }
+  }
+  // console.log('urls', urls);
+
   let templateVars = { 
     title: title, 
-    urls: urlDatabase,
+    urls: urls,//urlDatabase[req.cookies.user_id],
     id: req.cookies.user_id,
     email: req.cookies.user_email
   };
   res.render('index', templateVars);
-  // console.log(title, templateVars);
+  console.log(title, templateVars);
 });
 
 app.get('/urls/new', (req, res) =>{
-
   let templateVars={
-    user_id: req.cookies["username"] 
-  };
+    user_id: req.cookies["user_id"], 
+    id:req.cookies.user_id,
+    email: req.cookies.user_email
+  }; if(!req.cookies["user_email"]){
+    res.redirect('/urls');
+  }
 
   res.render('urls_new', templateVars);
 });
 
-app.post('/urls/new', (req, res)=>{
-  //console.log(req.body);
-  let key = generateRandomString();
-  urlDatabase[key] = req.body.longURL;
+app.post('/urls/new', (req, res) =>{
+  urlDatabase[generateRandomString()] = { url: req.body.longURL, user_id: req.cookies.user_id };
+  res.cookie('longURL', req.body.longURL);
   res.redirect('/urls');
 });
+
 app.post('/urls/:id/delete', (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
+  let longURL = urlDatabase[req.params.shortURL].user_id;
   res.redirect(longURL);
 });
 
@@ -74,8 +93,10 @@ app.get("/u/:shortURL", (req, res) => {
 app.get('/urls/:id', (req, res) =>{
   let templateVars = {
     shortURL: req.params.id,
-    urls: urlDatabase,
-    username: req.cookies["username"] 
+    urls: urlDatabase[req.cookies.user_id],
+    username: req.cookies["username"],
+    id:req.cookies.user_id,
+    email: req.cookies.user_email,
   };
 
   res.render('urls_show', templateVars);
@@ -83,7 +104,8 @@ app.get('/urls/:id', (req, res) =>{
 
 app.post('/urls/:id/update', (req, res) =>{
   let newURL = req.body.newURL;
-    urlDatabase[req.params.id] = newURL;
+    urlDatabase[req.params.id].url = newURL;
+    console.log(req.cookies.shortURL, newURL);
   res.redirect('/urls');
 });
 
@@ -92,7 +114,7 @@ app.post('/login', (req, res) => {
     res.status(400).send("You need to fill the blanks with text and/or numbers");
   }
   for(var ids in users){
-    if(users[ids].email === req.body.username && users[ids].password === req.body.password){
+    if(users[ids].email === req.body.username && bcrypt.compareSync(req.body.password, users[ids].password)) {
        res.cookie('user_id', ids);
         res.cookie('user_email', req.body.username);
       return res.redirect('/urls');
@@ -134,7 +156,7 @@ app.post('/register', (req, res)=>{
     res.status(400).send("You need to fill the blanks with text and/or numbers");
   } else {
 let email = req.body.email;
-let password =  req.body.password;
+let password =  hashPassword(req.body.password);
 let username = req.body.username;
 let id = generateRandomString();
         let person = {
@@ -143,7 +165,7 @@ let id = generateRandomString();
           password: password
         };
     users[id] = person;
-  res.cookie("user_id", id);
+    res.cookie("user_id", id);
     res.cookie("user_email", email);
     console.log(users);
     }
